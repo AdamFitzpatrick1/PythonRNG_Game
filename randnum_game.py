9de695519046999f0bf95564a6c591214a33fa5c
AdamFitzpatrick1 <31987448+AdamFitzpatrick1@users.noreply.github.com>
2020-01-22 16:05:05 +0000

    Update randnum_game.py

--- randnum_game.py
+++ randnum_game.py
@@ -1,28 +1,25 @@
-import random 
-import sys
+import random #imports the random number generator library.
+import sys #imports the system library.
 
 
 
 
 
-print("-----------------------------------------------------------------")
-print("W E L C O M E  T O  T H E  N U M B E R  G U E S S I N G  G A M E ")
-print("-----------------------------------------------------------------")
+print("-----------------------------------------------------------------")    #Prints the top part of a border
+print("W E L C O M E  T O  T H E  N U M B E R  G U E S S I N G  G A M E ")    #Prints the main text displayed for the welcome message
+print("-----------------------------------------------------------------")    #Prints the bottom part of a border
 
-print("Please choose an input from the folowing list of options: ")
-print()
-print("1. Play the Game")
-print("2. Exit the Game")
+print() #Empty print statement(just prints the text below on a new line, provides whitespace)
 
 
 
-for i in range(1,6):
-         randomnumber=random.randint(1,100)
-         gameinput = int(input("Please guess a number between 1 and 100 "))
-         if gameinput == randomnumber:
-             print("Your guess is correct!")
+for i in range(1,6): #A for loop which gives the user 5 chances to enter the correct answer. As computers start counting from 0 its techically counting from 0-4 which still alows for 5 chances.
+         randomnumber=random.randint(1,100) #Initialise the random number function and set it to create a random number in the range 1 t0 100.
+         gameinput = int(input("Please guess a number between 1 and 100 ")) #User input using the keyboard as an integer it instructs them to enter a guess
+         if gameinput == randomnumber: #An if statement that compares what the user has inputted to the previously generated random number
+             print("Your guess is correct!") #When the if statement condiiton is true, it will print that the user is correct
          else:
-             print("Better luck next time!")
+             print("Better luck next time!") #If it's not true (false) it will print better luck next time!
  
  
  
